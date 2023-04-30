743ec71c1315745e4899787c9bbd518756516444
Laptop-Limjihoon <lonelynight1026@gmail.com>
2023-04-30 12:15:26 +0900

    feat : 모든 경로에 logging 미들웨어 적용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AutoCompleteModule } from './auto-complete/auto-complete.module';
+import { LoggerMiddleware } from './logger/logger.middleware';
 
 @Module({
   imports: [AutoCompleteModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
